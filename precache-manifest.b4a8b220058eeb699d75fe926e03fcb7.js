31eab3ccaf147945a52dfaf0c13f94f359ae8d19
azuha07 <azuha07i@icloud.com>
2019-12-11 05:15:13 +0000

    Updates

---
+++ precache-manifest.b4a8b220058eeb699d75fe926e03fcb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4ea7cdf146ddfde58c",
+    "url": "/css/chunk-vendors.6efdd108.css"
+  },
+  {
+    "revision": "3bef0f17a921fd0ca4fe4f7d5378fa6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efd92b9969b7fb9d00bd",
+    "url": "/js/app.b4d54df1.js"
+  },
+  {
+    "revision": "4e4ea7cdf146ddfde58c",
+    "url": "/js/chunk-vendors.79439c25.js"
+  },
+  {
+    "revision": "0c9e30c5a47fb3293009debd5710e235",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
