5d5043afd3c39b536ff309bcb5b85fe8d19f0dd6
azuha07 <azuha07i@icloud.com>
2019-12-11 03:33:51 +0000

    create TypeChecker.vue

---
+++ src/components/pages/TypeChecker.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="type-checker">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TypeChecker',
+  data: () => ({
+    msg: 'TypeChecker'
+  })
+}
+</script>
+
+<style lang="scss">
+</style>
